59f2ffa4d437ab4b76f96cd49fd736a4e8def96f
kalindiwst <kalindiwst@users.noreply.github.com>
2026-04-01 15:42:50 -0400

    Add PWA: manifest, service worker, app icons, install support

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'team-viewer-v1';
+const ASSETS = [
+  '/team-viewer/',
+  '/team-viewer/index.html',
+  '/team-viewer/my-team.html',
+  '/team-viewer/manifest.json',
+  '/team-viewer/icons/icon-192.svg',
+  '/team-viewer/icons/icon-512.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
